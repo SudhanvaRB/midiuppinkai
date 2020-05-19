afb12b5bbaf1834f9f90fab607dc78ee1514a0ca
Sudhanva <sudbhara@cisco.com>
2020-05-19 20:20:08 +0530

    changed 3 files

--- newblog.js
+++ newblog.js
@@ -6,3 +6,13 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+function openTitle() {
+ const container = document.querySelector('#container');
+ const content = document.createElement('div');
+ content.classList.add('content');
+ content.textContent = "this is text content";
+ container.appendChild(content);
+ content.style.cssText = 'color: white; font-size: 4rem; background: red ; padding-left:150px'; 
+
+}
